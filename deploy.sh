5ed9fbb3b23429567e4ee580871377363700c9a4
Lee-chan0 <yab0403@kakao.com>
2023-12-21 05:19:17 +0900

    docker7

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 # deploy.sh
+
 # 이미지 이름은 Docker Hub에 있는 계정 및 프로젝트에 맞게 수정
 DOCKER_USERNAME=leechan0
 IMAGE_NAME=$DOCKER_USERNAME/app
+
+# Docker 설치
+sudo apt-get update
+sudo apt-get install -y docker.io
 
 # Docker 이미지 빌드
 docker build -t $IMAGE_NAME .
