9f06530bf3607a4f1b8e9a0caf2de134b9d6697e
Lee-chan0 <yab0403@kakao.com>
2023-12-21 19:03:19 +0900

    docker 16

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,22 @@
-# deploy.sh
-
-# 이미지 이름은 Docker Hub에 있는 계정 및 프로젝트에 맞게 수정
+# 예: deploy.sh 스크립트
 DOCKER_USERNAME=leechan0
 IMAGE_NAME=$DOCKER_USERNAME/app
 
-# Docker 이미지 빌드
-docker build -t $IMAGE_NAME .
+# 이미지 빌드
+docker build -t $IMAGE_NAME:$GITHUB_SHA .
 
 # Docker Hub에 로그인
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Docker 이미지 푸시
-docker push $IMAGE_NAME
+docker push $IMAGE_NAME:$GITHUB_SHA
+
+# Nginx 업스트림 변경
+sed -i 's/backend-blue/backend-green/' /etc/nginx/nginx.conf
+sed -i 's/3000/4000/' /etc/nginx/nginx.conf  # 포트 변경
+
+# Nginx 서버 재시작
+sudo service nginx restart
 
 # Docker 컨테이너 실행 (예시)
-docker run -d -p 3000:3000 $IMAGE_NAME
-
-# Nginx 서버 업데이트 및 재시작
-sudo service nginx restart
+docker run -d -p 3000:3000 $IMAGE_NAME:$GITHUB_SHA  # 포트 변경
--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,11 +1,22 @@
 REPOSITORY=/home/ubuntu/finalCICD
+BLUE_SERVER=$REPOSITORY/blue
+GREEN_SERVER=$REPOSITORY/green
+CURRENT_SERVER=$REPOSITORY/current
 
+# 현재 서버를 'current'로 지정
+ln -snf $BLUE_SERVER $CURRENT_SERVER
+
+# 'current' 서버의 포트 확인
+CURRENT_PORT=$(sudo lsof -t -i:3000)
+
+# 서비스 중단
+sudo kill -s SIGTERM $CURRENT_PORT
+
+# 새로운 서버 빌드 및 실행
 cd $REPOSITORY
-
 chmod +x deploy.sh
-
 sudo npm ci
-
 ./deploy.sh
 
-npm start
+# 서비스 시작
+sudo npm start
