b1a01adc730478a047917c286551ce5516795a6c
Lee-chan0 <yab0403@kakao.com>
2023-12-24 03:45:43 +0900

    f

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use("/", [userRouter]);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("lllllllffffffffffffffffffoooooooooolll");
+  res.send("lets go");
 });
 
 // 헬스 체크 엔드포인트 추가
